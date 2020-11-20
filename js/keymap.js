ede9cf775b2d5d6ec625f5fc283bc1c6c0de26b1
ExpandingDev <javscripter@yahoo.com>
2020-11-19 19:01:24 -0500

    Completed single note feature

--- js/keymap.js
+++ js/keymap.js
@@ -1,16 +1,179 @@
-var keymap = {
-	'q':"D3",
-	'a':"E3",
-	's':"F3",
-	'd':"G3",
-	'f':"A3",
-	'g':"B3",
-	'h':"C4",
-	'j':"D4",
-	'k':"E4",
-	'l':"F4",
-	';':"G4",
-	'\'':"A4",
-	']':"B4",
-	'\\':"C5"
-};
+
+/**
+	keymap.js	Author: Tyler Sengia
+	Contains the class and Vue component for a configurable piano keymapping.
+	
+	How this works:
+	   You map keys to action names. (using setKey)
+	   You can they assign callback functions to action names. (addActionListener)
+	   There can be multiple callback functions per action name.
+	   Each callback function will be called with the following parameters:
+	       callback(string actionName, boolean keyDown)
+       
+       key -> Action name -> CallbackA & CallbackB & Callback...
+       
+       Why have action names? 
+       This allows you to change the key assigned to the note without rebuilding your callbacks.
+       This also allows you to set action listeners without knowing the specific key that is bound to an action.
+*/
+
+class PianoKeymap {
+
+	///Registers keydown and keyup event listeners
+	apply() {
+		if(!this._applied) {
+			window.addEventListener("keydown", this._keydownCallback.bind(this));
+			window.addEventListener("keyup", this._keyupCallback.bind(this));
+			this._applied = true;
+		}
+	}
+
+	///Unregisters keydown and keyup event listeners
+	rollback() {
+		if(this._applied) {
+			window.removeEventListener("keydown", this._keydownCallback);
+			window.removeEventListener("keyup", this._keyupCallback);
+			this._applied = false;
+		}
+	}
+
+	///Either creates a new action/key pair or updates an existing one
+	///Returns true if the key conflicts with already existing key/action pairs
+	setKey(actionName, keyValue) {
+		keyValue = keyValue.charCodeAt(0);
+		let j = this.indexOfAction(actionName);
+		if(j != -1) {
+			this.actions[j].setting = keyValue;
+		}
+		else {
+			this.actions.push({ id: this._actionID, setting: keyValue, actionName: actionName });
+			this._actionID++;
+		}
+
+		var count = 0;
+		for(let i = 0; i < this.actions.length; i++) {
+			if(this.actions[i].setting == keyValue) {
+				this.count++;
+			}
+		}
+		return count > 1;
+	}
+
+	///Removes an action/key pair from the action list
+	removeAction(actionName) {
+		let i = this.indexOfAction(actionName);
+		if(i != -1) {
+			this.actions.splice(i, 1);
+		}
+	}
+
+	addActionListener(actionName, callback) {
+		if(actionName in this._callbacks) {
+			this._callbacks[actionName].push(callback);
+		}
+		else {
+			this._callbacks[actionName] = [];
+			this._callbacks[actionName].push(callback);
+		}
+	}
+
+	indexOfAction(actionName) {
+		for(let i = 0; i < this.actions.length; i++) {
+			if(this.actions[i].actionName == actionName) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	_keydownCallback(e) {
+		if(this._keysDown.indexOf(e.key.charCodeAt(0)) == -1) {
+			this._keysDown.push(e.key.charCodeAt(0));
+			for(let p in this.actions) {
+				if(this.actions[p].setting == e.key.charCodeAt(0)) {
+					e.preventDefault();
+					let actionName = this.actions[p].actionName;
+					if(actionName in this._callbacks) {
+						for(let i = 0; i < this._callbacks[actionName].length; i++) {
+							this._callbacks[actionName][i](actionName, true);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	_keyupCallback(e) {
+		let i = this._keysDown.indexOf(e.key.charCodeAt(0));
+		if(i != -1) {
+			delete this._keysDown[i];
+		}
+
+		for(let p in this.actions) {
+			if(this.actions[p].setting == e.key.charCodeAt(0)) {
+				let actionName = this.actions[p].actionName;
+				if(actionName in this._callbacks) {
+					for(let i = 0; i < this._callbacks[actionName].length; i++) {
+						this._callbacks[actionName][i](actionName, false);
+					}
+				}
+			}
+		}
+	}
+
+	constructor() {
+		this._actionID = 0;
+		this.actions = [];
+		this._callbacks = [];
+		this._keysDown = [];
+		this._applied = false;
+	}
+}
+
+Vue.component("keymap-window", {
+	props: {
+		keymap: Object
+	},
+	data: function() { return {
+		windowOpen: false
+	}},
+	template: "<span><div class='keymap-window' v-if='windowOpen' >\
+	<div class='title-bar' ><button type='button' v-on:click='windowOpen = false' >X</button>Keys</div><div class='window-content'>\
+	<keymap-entry v-for='k in keymap.actions' :key='k.id' v-bind:actual='k' v-bind:action-name='k.actionName' v-bind:setting='k.setting' />\
+	</div></div><button type='button' v-on:click='windowOpen=true' >Keys</button></span>"
+});
+
+// Each key that is bound to a note is known as an entry
+Vue.component("keymap-entry", {
+	props: {
+		actionName: String,
+		actual: Object
+	},
+	data: function () { return {
+		editing: false
+	}},
+	methods: {
+		setKey: function() {
+			this.editing = true;
+			window.addEventListener("keydown", keydownSetListener = (e) => {
+				this.setting = e.key.charCodeAt(0);
+				this.editing = false;
+				window.removeEventListener("keydown", keydownSetListener);
+			});
+		},
+	},
+	computed: {
+		displayedSetting: function() {
+			return String.fromCharCode(this.setting);
+		},
+		setting: {
+			get: function() {
+				return this.actual.setting;
+			},
+			set: function(val) {
+				this.actual.setting = val;
+			}
+		}
+	},
+	template: "<div v-on:click='setKey' class='keymap-setting' >{{ actionName }} : <span v-if='editing' >-?-</span><span v-if='!editing' >{{ displayedSetting }}</span></div>"
+});
---
+++ js/main_page.js
@@ -0,0 +1,227 @@
+/**
+	main_page.js
+*/
+
+var app = new Vue({
+	el: "#app",
+	data: {
+		keymapEditorOpen: false,
+		keymap: new PianoKeymap(),
+		hint: "",
+		showHint: false,
+		showFail: false,
+		showSuccess: false,
+		loaded: false,
+		abcString: "C",
+		abcScale: 1,
+		notesOn: [],
+		showNotesOn: true,
+		contentText: "",
+		modes: [ { name: "Free Play", id: 0},
+			 { name: "Single Note", id: 1},
+			 { name: "Song", id: 2},
+			 { name: "Scales", id: 3}],
+	    singleNoteRanges: [{ name: "All Notes", id:0 }, 
+	    { name: "C4", id:1 }, { name: "B3-D4", id: 2 }, { name: "A3-E4", id: 3 }, { name: "G3-F4", id: 4 }
+	    ],
+	    selectedNoteRange: 0,
+		selectedMode: "0",
+		songs: [ { name: "Happy Birthday", id:0 } ],
+		selectedSong: "0",
+		scales: [ 
+		  { name: "C Major", id: 0}, { name: "C Minor", id: 1}
+		],
+		selectedScale: "0",
+		showSongSelection: false,
+		boundNotes: [],
+		boundNoteRange: null,
+		selectedNote: "C4"
+	},
+	watch: {
+		abcString: function(newVal, oldVal) {
+			ABCJS.renderAbc("music-page", newVal, { scale: this.abcScale });
+		},
+		selectedNoteRange: function (newVal, oldVal) {
+		    this.displayRandomNote();
+		},
+		selectedMode: function(newVal, oldVal) {
+		    this.showSongSelection = false;
+		    if (newVal == "3") {
+		        this.showScaleSelection = true;
+		        this.showScale(this.selectedScale);
+		    }
+			else if(newVal == "2") {
+				this.showSongSelection = true;
+				this.loadSong(this.selectedSong);
+			}
+			else if (newVal == "1") { // Single Note
+			    this.displayRandomNote();
+			}
+			else if (newVal == "0") { // Free play
+			     this.showFail = false;
+			     this.showSuccess = false;
+			}
+		},
+		selectedSong: function(newSong, oldSong) {
+			this.loadSong(newSong);
+		}
+	},
+	methods: {
+        showScale: function (selectedScale) {
+            console.log(selectedScale);
+            this.selectedNote = Scales[this.scales[selectedScale].name][0];
+        },
+        displayRandomNote: function () {    
+            if (this.selectedNoteRange == 0) {
+                this.selectedNote = this.boundNoteRange.getRandomNote();
+            }
+            else if (this.selectedNoteRange == 1) {
+                this.selectedNote = new Note("C", 4);
+            }
+            else {
+                var i = (new Note("C", 4)).getNoteAtDistance(1-this.selectedNoteRange);
+                var x = (new Note("C", 4)).getNoteAtDistance(this.selectedNoteRange-1);
+                var range = new NoteRange(i,x);
+                this.selectedNote = range.getRandomNote();
+            }
+              
+            this.selectedNote.duration = "4";
+
+            var s = "M: 4/4\nK:C\n[V:1 cl=treble] ";
+            if (ClefRanges.treble.inRange(this.selectedNote) > 0) {
+                s += this.selectedNote.toABC() + " [V:2 cl=bass] z4";
+                this.abcString = s;
+                this.abcScale = 2;
+            }
+            else if (ClefRanges.bass.inRange(this.selectedNote) > 0) {  
+                s += "z4 [V:2 cl=bass] " + this.selectedNote.toABC();
+                this.abcString = s;
+                this.abcScale = 2;
+            }
+            else {
+                console.log("BARG");
+                //this.displayRandomNote();            
+            }
+        },
+	    loadSoundFont: function () {
+	        this.hint = "Loading...";
+	        MIDI.loadPlugin({
+			soundfontUrl: "./soundfonts/",
+			api:"flash",
+			instrument: "acoustic_grand_piano",
+			onerror: function() {
+				console.log("Failed to load soundfont.");
+				app.hint = "Failed to load.";
+				app.showHint = true;
+			},
+			onsuccess: function() {
+				MIDI.setVolume(0, 200);
+				app.showHint = false;
+				app.loaded = true;
+				console.log("Soundfont loaded.");
+			}
+		  });
+	    },
+		loadSong: function(id) {
+			this.hint = "Loading...";
+			this.showHint = true;
+			axios({
+				method: "get",
+				url: "songs/" + id + ".abc",
+				responseType: "text"
+			}).then(function (response) {
+				app.showHint = false;
+				app.abcString = response.data;
+			}).catch(function(error) {
+				app.abcString = "";
+				app.hint = "ERROR LOADING SONG";
+				app.showHint = true;
+			});
+		},
+		renderMusic: function() {
+			ABCJS.renderAbc("music-page", this.abcString, { scale: this.abcScale });
+		},
+		openKeymapEditor: function() {
+			if(!this.keymapEditorOpen) {
+				this.keymapEditorOpen = true;
+			}
+		},
+		closeKeymapEditor: function() {
+			if(this.keymapEditorOpen) {
+				this.keymapEditorOpen = false;
+			}
+		},
+		pianoKeyDown: function(note) {
+			let m = MIDI.keyToNote[note];
+			MIDI.noteOn(0,m,127,0);
+			if (this.selectedMode == "1") { // Single note playing
+			    if(this.selectedNote.toMIDI() == m) {
+			        this.showFail = false;
+			        this.showSuccess = true;
+                    this.displayRandomNote();			    
+			    }
+			    else {
+                    this.showSuccess = false;
+                    this.showFail = true;			    
+			    }
+		    }
+		},
+		pianoKeyUp: function(note) {
+			let m = MIDI.keyToNote[note];
+			MIDI.noteOff(0, m, 0);
+			this.showSuccess = false;
+			this.showFail = false;
+		},
+		pianoKeyEventCallback: function(note, isDown) {
+			if(isDown) {
+				this.pianoKeyDown(note);
+				this.notesOn.push(note);
+			}
+			else {
+				this.pianoKeyUp(note);
+				this.notesOn.splice(this.notesOn.indexOf(note),1);
+			}
+		}
+	},
+	mounted: function () {
+		// Set default keymapping. TODO: Make this load in from cookies.
+		this.keymap.setKey("D3", "q");
+		this.keymap.setKey("E3", "a");
+		this.keymap.setKey("F3", "s");
+		this.keymap.setKey("G3", "d");
+		this.keymap.setKey("A3", "f");
+		this.keymap.setKey("B3", "g");
+		this.keymap.setKey("C4", "h");
+		this.keymap.setKey("D4", "j");
+		this.keymap.setKey("E4", "k");
+		this.keymap.setKey("F4", "l");
+		this.keymap.setKey("G4", ";");
+		this.keymap.setKey("A4", "'");
+		this.keymap.setKey("B4", "]");
+		this.keymap.setKey("C5", "\\");
+		this.boundNotes = ["D3","E3","F3","G3","A3","B3","C4","D4","E4","F4","G4","A4","B4","C5"];
+        this.boundNoteRange = new NoteRange(new Note("D",3), new Note("C",5));
+
+		this.keymap.addActionListener("D3", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("E3", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("F3", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("G3", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("A3", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("B3", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("C4", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("D4", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("E4", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("F4", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("G4", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("A4", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("B4", this.pianoKeyEventCallback);
+		this.keymap.addActionListener("C5", this.pianoKeyEventCallback);
+
+		this.keymap.apply();
+
+		ABCJS.renderAbc("music-page", this.abcString, { scale: this.abcScale });
+
+		this.hint = "Press Load Soundfont.";
+		this.showHint = true;
+	}
+});
